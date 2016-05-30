b021ac7a29a6d1943a2ac7cf23556a1e461239f9
Kevin Filipe <kevinfillipe@gmail.com>
2016-05-30 15:18:07 -0300

    Adiciona suporte a CORS no backend

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,10 @@ var router = express.Router();
 
 router.use(function(req, res, next) {
     console.log('Requisao \'' + req.method + '\' para o recurso \'' + req.originalUrl + '\' recebida...');
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    
     next();
 });
 
