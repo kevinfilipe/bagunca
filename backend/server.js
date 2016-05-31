0f883381d3c8b1d539922b120ced1c3492f41862
Kevin Filipe <kevinfillipe@gmail.com>
2016-05-31 13:19:48 -0300

    Atualiza formacao do cabecalho para requisicoes CORS

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ router.use(function(req, res, next) {
 
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     
     next();
 });
